2b655f364a1d78ef065df7b8822d33dc585563c7
Joel Chen <xchen@walmartlabs.com>
2018-11-16 08:08:45 -0800

    auto dirname if file path is passed

--- require-at.js
+++ require-at.js
@@ -10,12 +10,13 @@ const orig_findPath = Module._findPath;
 const contextExt = "._require_at_";
 const contextFname = `._hook_${contextExt}`;
 
-Module._findPath = function (request, paths, isMain) {
-  return request.endsWith(contextFname) ? request :
-    orig_findPath.call(Module, request, paths, isMain);
+Module._findPath = function(request, paths, isMain) {
+  return request.endsWith(contextFname)
+    ? request
+    : orig_findPath.call(Module, request, paths, isMain);
 };
 
-Module._extensions[contextExt] = function (module, filename) {
+Module._extensions[contextExt] = function(module, filename) {
   module._compile("module.exports = require;", filename);
 };
 
@@ -26,17 +27,35 @@ const contextMap = new Map();
 function requireAt(dir, request) {
   let xRequire;
   dir = Path.resolve(dir);
-  if (!contextMap.has(dir)) {
-    if (!Fs.existsSync(dir)) {
-      throw new Error(`require-at: dir '${dir}' doesn't exist`);
-    }
-    xRequire = require(Path.join(dir, contextFname));
-    contextMap.set(dir, xRequire);
-  } else {
-    xRequire = contextMap.get(dir);
-  }
 
-  return request ? xRequire(request) : xRequire;
+  let dirChecked = false;
+
+  const makeIt = () => {
+    if (!contextMap.has(dir)) {
+      let stat;
+      try {
+        stat = Fs.statSync(dir);
+      } catch (e) {
+        throw new Error(`require-at: stat '${dir}' failed: ${e.message}`);
+      }
+
+      if (!dirChecked && !stat.isDirectory()) {
+        dirChecked = true;
+        dir = Path.dirname(dir);
+        return makeIt();
+      }
+
+      xRequire = require(Path.join(dir, contextFname));
+
+      contextMap.set(dir, xRequire);
+    } else {
+      xRequire = contextMap.get(dir);
+    }
+
+    return request ? xRequire(request) : xRequire;
+  };
+
+  return makeIt();
 }
 
 module.exports = requireAt;
--- test/spec/require-at.spec.js
+++ test/spec/require-at.spec.js
@@ -2,16 +2,16 @@ const Path = require("path");
 const requireAt = require("../..");
 const chai = require("chai");
 
-describe("require-at", function () {
+describe("require-at", function() {
   it("should be able to require at another dir as relative dir", () => {
     const appRequire = requireAt("test/fixtures/app");
-    const foo = appRequire("foo")
+    const foo = appRequire("foo");
     chai.expect(foo).to.equal("foo");
   });
 
   it("should be able to require at another dir as absolute dir", () => {
     const appRequire = requireAt(Path.resolve("test/fixtures/app"));
-    const foo = appRequire("foo")
+    const foo = appRequire("foo");
     chai.expect(foo).to.equal("foo");
   });
 
@@ -20,7 +20,12 @@ describe("require-at", function () {
   });
 
   it("should require a file directly if passed", () => {
-    const foo = requireAt("test/fixtures/app", "foo")
+    const foo = requireAt("test/fixtures/app", "foo");
+    chai.expect(foo).to.equal("foo");
+  });
+
+  it("should auto dirname on a file that's passed", () => {
+    const foo = requireAt("test/fixtures/app/bar.js", "foo");
     chai.expect(foo).to.equal("foo");
   });
 });
